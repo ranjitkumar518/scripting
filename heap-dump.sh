16f5c32350228a809ea7ab12dc26042385e07811
ranjitkumar518 <ranjitkumar.518@gmail.com>
2018-09-11 15:11:49 -0700

    Update heap-dump.sh

--- heap-dump.sh
+++ heap-dump.sh
@@ -2,5 +2,6 @@ while true
 do
   # /usr/local/java/bin/jmap -dump:format=b,file=filename.hprof $PID
   jmap -dump:file=/tmp/java-`date +%s`.hprof PID_OF_JVM
+  # Sleep for 30 seconds and take the heap dump again.
   sleep 30
 done
