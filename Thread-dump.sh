51d9d0ab5babd92bcf5233e20cc901847ca1d320
ranjitkumar518 <ranjitkumar.518@gmail.com>
2018-09-12 14:16:19 -0700

    Update Thread-dump.sh

--- Thread-dump.sh
+++ Thread-dump.sh
@@ -4,6 +4,8 @@ while true
 do
   # /usr/java/jdk1.8.0_131/bin/jstack is the Jstack Home directory
   jstack $1 > stack-`date +%s`.txt
+  ## run command 5 times  
+  ## for i in {1..5}; do /usr/java/jdk1.8.0_131/bin/jstack $1 > stack-`date +%s`.txt ; done
   # Sleep for 30 seconds to execute the same script
   sleep 30
 done
