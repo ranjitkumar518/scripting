192da9d8996fb72fd1d63f5bed0e67a53436623d
ranjitkumar518 <ranjitkumar.518@gmail.com>
2018-09-19 19:27:58 -0700

    Print * instead of dot

--- Draw-pattern.sh
+++ Draw-pattern.sh
@@ -12,7 +12,7 @@ for (( i=1; i<=MAX_NO; i++ )) do     for (( s=MAX_NO; s>=i; s-- ))
 do
 echo -n " "
 done
-for (( j=1; j<=i;  j++ ))     do      echo -n " ."      done     echo "" done ###### Stage Two ###################### for (( i=MAX_NO; i>=1; i-- ))
+for (( j=1; j<=i;  j++ ))     do      echo -n " *"      done     echo "" done ###### Stage Two ###################### for (( i=MAX_NO; i>=1; i-- ))
 do
 for (( s=i; s<=MAX_NO; s++ ))
 do
