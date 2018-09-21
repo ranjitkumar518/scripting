20b858f474be99d5b3ba72f907a3667c25f4ec40
ranjitkumar518 <ranjitkumar.518@gmail.com>
2018-09-20 18:04:53 -0700

    Update Thread-dump.sh

--- Thread-dump.sh
+++ Thread-dump.sh
@@ -9,3 +9,6 @@ do
   # Sleep for 30 seconds to execute the same script
   sleep 30
 done
+
+# Command to take Thread dump for every 10 seconds.
+#CLUSTER_NAME=$(hostname|grep -o -E c[0-9]{2});echo ${CLUSTER_NAME};COUNTER=0 ; mkdir -p ${CLUSTER_NAME};while true; do sleep 10; ((COUNTER++));echo "Taking thread dump..." ${COUNTER}; /usr/java/jdk1.8.0_131/bin/jstack -l $pid >  ${CLUSTER_NAME}/dumps_$(date "+%F%T"|sed 's/://g') ;if [ ${COUNTER} -eq 5 ]; then  tar -cvzf  ${CLUSTER_NAME}.tar ${CLUSTER_NAME}; fi; done
