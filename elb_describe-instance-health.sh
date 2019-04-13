d6afc485348d469e75b6d905fff79417571ef275
Ranjit G <ranjitkumar.518@gmail.com>
2019-04-12 17:34:51 -0700

    Create elb_describe-instance-health.sh

---
+++ elb_describe-instance-health.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 3 ]; then
+      echo "Usage: ./check_elb_instance_health.sh <ENV> <AWS_PROFILE> <COMPONENT>"
+      echo "Required: ENV, AWS_PROFILE, COMPONENT"
+      echo "Example: ./check_elb_instance_health.sh prf2 preprod web-app"
+      echo "Example: ./check_elb_instance_health.sh prf2-green preprod web-app"
+      exit
+fi
+
+ENV=$1
+AWS_PROFILE=$2
+COMPONENT=$3
+
+echo --- Healthy instances to the ELB $COMPONENT-$ENV ---
+aws elb describe-instance-health --profile $AWS_PROFILE \
+    --load-balancer-name $COMPONENT-$ENV \
+    --query 'InstanceStates[?State==`InService`].[InstanceId,State]' \
+    --output table
+
+echo --- Unhealthy instances to the ELB $COMPONENT-$ENV ---
+aws elb describe-instance-health --profile $AWS_PROFILE \
+    --load-balancer-name $COMPONENT-$ENV \
+    --query 'InstanceStates[?State!=`InService`].[InstanceId,State]' \
+    --output table
