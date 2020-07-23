10760e03e39f75358aed1510e14d2c4e69ba8417
Ranjit G <ranjitkumar.518@gmail.com>
2020-07-23 14:24:57 -0700

    Update Cron-lastdayofmonth.sh

--- Cron-lastdayofmonth.sh
+++ Cron-lastdayofmonth.sh
@@ -1,2 +1,2 @@
 ## command to run cron job at the last day of the month
-[ $(date +\%d -d tomorrow) = 01 ] && /root/audit_report.sh >> /var/log/audit_report.log 2>&1
+[ $(date +\%d -d tomorrow) = 01 ] && /root/run_report.sh >> /var/log/report.log 2>&1
